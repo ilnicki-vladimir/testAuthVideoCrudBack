f67a72847ae5cea4b97058da967683d8e0c518e3
Uladzimir Ilnitski <uladzimir.ilnitski@innowise-group.com>
2020-11-02 15:32:09 +0300

    fix swagger https url

--- src/loaders/swagger.ts
+++ src/loaders/swagger.ts
@@ -10,10 +10,10 @@ export default (app: express.Application):void => {
         title: 'Swagger',
         version: '1.0.0',
       },
-      host: 'https://back-end-test-video-crud.herokuapp.com',
+      host: 'back-end-test-video-crud.herokuapp.com',
       basePath: '/',
       produces: ['application/json', 'application/xml'],
-      schemes: ['http', 'https'],
+      schemes: ['https', 'http'],
       securityDefinitions: {
         JWT: {
           type: 'apiKey',
